1188e3755f3355edf8e717e37b12b0e196cda37d
Ruikaj <kajrui1025@gmail.com>
2025-11-17 02:34:15 +0900

    StoreSongInfoの出力文字列フォーマットの終端文字$00を改行

--- Source/ChunkRenderText.cpp
+++ Source/ChunkRenderText.cpp
@@ -1024,9 +1024,9 @@ void CChunkRenderText::StoreSongInfo(const CStringA& name, const CStringA& artis
 	CStringA str;
 	str.Format(
 		"; Song information\n\n"
-		"song_title:    .byte \"%s\" .byte $00\n" 
-		"song_artist:   .byte \"%s\" .byte $00\n"
-		"song_copyright:.byte \"%s\" .byte $00\n",
+		"song_title:    .byte \"%s\" \n.byte $00\n" 
+		"song_artist:   .byte \"%s\" \n.byte $00\n"
+		"song_copyright:.byte \"%s\" \n.byte $00\n",
 		(LPCSTR)name, (LPCSTR)artist, (LPCSTR)copyright);
 
 	WriteFileString(str, m_pFileSongInfo);
