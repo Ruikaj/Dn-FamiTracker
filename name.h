9e4c982f481558b9921f363b8d12501587d1805b
Ruikaj <kajrui1025@gmail.com>
2026-04-09 02:29:18 +0900

    name変更

--- name.h
+++ name.h
@@ -1,2 +1,2 @@
 #pragma once
-#define APP_NAME	"Dn-FamiTracker (Exportable total instrument size:12KB, max instrument index = 127, max DPCM index = 127  v20251102)"
+#define APP_NAME	"Dn-FamiTracker (Exportable total instrument size:8KB, max instrument index = 127, max DPCM index = 127  v20260409)"
